2408b1b766052ea1be043def4ebb85fda301c80b
Triggerhappy247 <qasim.bhatia@gmail.com>
2017-04-24 11:28:26 +0000

    Implemeted load function for trie

--- dictionary.c
+++ dictionary.c
@@ -3,8 +3,35 @@
  */
 
 #include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+#define LOWERCASE_A 97
+#define APOSTROPHE 26
 
 #include "dictionary.h"
+
+typedef struct _trie
+{
+    bool isWord;
+    struct _trie *children[27];
+} node ;
+
+// Declare root node
+node *root = malloc(sizeof(node));
+int dicWordCount = 0;
+
+/**
+ * Returns the index of a letter in a node
+ */
+int getindex(const char ch)
+{
+    if(ch == '\'')
+        return APOSTROPHE;
+    else
+        return tolower(ch) - LOWERCASE_A ;
+}
 
 /**
  * Returns true if word is in dictionary else false.
@@ -20,8 +47,38 @@ bool check(const char *word)
  */
 bool load(const char *dictionary)
 {
-    // TODO
-    return false;
+    FILE *inptr = fopen(dictionary,"r");
+    char word[45];
+    int i, index;
+    node *newNode = root;
+    while(feof(inptr) == 0)
+    {
+        fscanf(inptr, "%s", word);
+        for(i = 0; word[i] != '\0', i++)
+        {
+            index = getindex(word[i]);
+            if(newNode->children[index] == NULL)
+            {
+                newNode->children[index] = malloc(sizeof(node));
+            
+                if(newNode->children[index] == NULL)
+                {
+                    return false;
+                }
+            }
+            newNode = newNode->children[index];
+        }
+        newNode->isWord = true;
+        dicWordCount++;
+    }
+    if (ferror(inptr))
+    {
+        fclose(inptr);
+        unload();
+        return false;
+    }
+    fclose(inptr);
+    return true;
 }
 
 /**
@@ -29,8 +86,7 @@ bool load(const char *dictionary)
  */
 unsigned int size(void)
 {
-    // TODO
-    return 0;
+    return dicWordCount;
 }
 
 /**
@@ -38,6 +94,5 @@ unsigned int size(void)
  */
 bool unload(void)
 {
-    // TODO
-    return false;
+    unload(root);
 }
